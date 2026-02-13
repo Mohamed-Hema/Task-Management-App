b6b50ff9f92d03dd5d394dce9686158a45ca0c6d
Mohamed-Hema <mohamedhema2017@gmail.com>
2026-02-13 22:24:49 +0200

    feat(homepage): basic to-do list and header with profile picture

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,44 @@
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const toDoData = [
+    { id: 1, title: "Buy groceries", isDone: false },
+    { id: 2, title: "Walk the dog", isDone: true },
+    { id: 3, title: "Finish project", isDone: false },
+    { id: 4, title: "Call mom", isDone: true },
+    { id: 5, title: "Read a book", isDone: false },
+    { id: 6, title: "Exercise", isDone: true },
+  ];
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello Hema</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Ionicons name="menu" size={24} color="#000" />
+        <Image
+          source={{ uri: "https://randomuser.me/api/portraits/men/1.jpg" }}
+        />
+      </View>
+      <FlatList
+        data={toDoData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  header: {
+    marginBottom: 20,
+  },
+});
